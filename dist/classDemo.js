26d760d766975dbe7881241af9bb21d389f81235
nabendu82 <nabendu.biswas@gmail.com>
2024-08-15 08:21:57 +0530

    Classes done

--- dist/classDemo.js
+++ dist/classDemo.js
@@ -13,18 +13,78 @@ class Room {
     showFamily() {
         console.log(this.family);
     }
+}
+class OyoRoom extends Room {
     cleanRoom(soap) {
-        console.log(`Cleaning ${this.room} with ${soap}`);
+        console.log(`${this.room} is Oyo Room cleaned with ${soap}`);
+    }
+    get allReports() {
+        return this.reports;
+    }
+    set newReport(report) {
+        this.reports.push(report);
+    }
+    constructor(room, roomRent) {
+        super(room);
+        this.roomRent = roomRent;
+        this.reports = [];
+    }
+    static createRoom(room) {
+        return { room: room };
+    }
+    addFamilyMember(member) {
+        if (member === 'Kapil')
+            return;
+        this.family.push(member);
+    }
+    changeRoomRent(rent) {
+        this.roomRent = rent;
+    }
+    showRoomRent() {
+        console.log(`${this.room}'s room rent is ${this.roomRent}`);
     }
 }
-const nabs = new Room('Nabendu');
-const mous = new Room('Mousam');
-const hris = new Room('Hriday');
-const shiks = new Room('Shikha');
-shiks.dobShikha;
-nabs.addFamilyMember('Nabendu');
-nabs.addFamilyMember('Mousam');
-nabs.addFamilyMember('Hriday');
-nabs.addFamilyMember('Shikha');
-nabs.showFamily();
-mous.cleanRoom('Lizol');
+OyoRoom.currentYear = 2024;
+class TreboHotel extends Room {
+    constructor(room, roomRent) {
+        super(room);
+        this.roomRent = roomRent;
+    }
+    static getInstance() {
+        if (!TreboHotel.instance)
+            TreboHotel.instance = new TreboHotel("Trebo", 1000);
+        return TreboHotel.instance;
+    }
+    cleanRoom(soap) {
+        console.log(`${this.room} is Trebo Room cleaned with ${soap}`);
+    }
+}
+const vijay = TreboHotel.getInstance();
+console.log(vijay);
+const rohit = OyoRoom.createRoom('Rohit');
+console.log(rohit);
+console.log(OyoRoom.currentYear);
+const shekar = new OyoRoom('Shekar', 1000);
+const shobha = new OyoRoom('Shobha', 1000);
+const kapil = new OyoRoom('Kapil', 1000);
+shekar.newReport = 'Year End Report';
+console.log(shekar.allReports);
+shekar.addFamilyMember('Shekar');
+shekar.addFamilyMember('Shobha');
+shekar.addFamilyMember('Kapil');
+shekar.showFamily();
+shekar.showRoomRent();
+shekar.changeRoomRent(2000);
+shekar.showRoomRent();
+shekar.cleanRoom('Domex');
+// const nabs = new Room('Nabendu');
+// const mous = new Room('Mousam');
+// const hris = new Room('Hriday');
+// const shiks = new Room('Shikha');
+// shiks.dobShikha;
+// nabs.addFamilyMember('Nabendu');
+// nabs.addFamilyMember('Mousam');
+// nabs.addFamilyMember('Hriday');
+// nabs.addFamilyMember('Shikha');
+// nabs.showFamily();
+// mous.cleanRoom('Lizol');
--- src/classDemo.ts
+++ src/classDemo.ts
@@ -1,6 +1,6 @@
 //Classes
-class Room {
-    private family: string[] = [];
+abstract class Room {
+    protected family: string[] = [];
     readonly dobShikha: string = '1982-12-12';
     private readonly dobHriday: string = '2013-12-12';
     constructor(public room: string){}
@@ -13,19 +13,91 @@ class Room {
         console.log(this.family);
     }
 
-    cleanRoom(soap: string){
-        console.log(`Cleaning ${this.room} with ${soap}`)
+    abstract cleanRoom(soap: string): void;
+}
+
+class OyoRoom extends Room {
+    private reports: string[] = [];
+    static currentYear = 2024;
+    cleanRoom(soap: string) {
+        console.log(`${this.room} is Oyo Room cleaned with ${soap}`)
+    }
+    get allReports(){
+        return this.reports;
+    }
+
+    set newReport(report: string){
+        this.reports.push(report);
+    }
+
+    constructor(room: string, private roomRent: number) {
+        super(room)
+    }
+
+    static createRoom(room: string){
+        return { room: room }
+    }
+
+    addFamilyMember(member: string) {
+        if(member === 'Kapil')
+            return
+        this.family.push(member);
+    }
+
+    changeRoomRent(rent: number){
+        this.roomRent = rent;
+    }
+
+    showRoomRent(){
+        console.log(`${this.room}'s room rent is ${this.roomRent}`)
     }
 }
 
-const nabs = new Room('Nabendu');
-const mous = new Room('Mousam');
-const hris = new Room('Hriday');
-const shiks = new Room('Shikha');
-shiks.dobShikha;
-nabs.addFamilyMember('Nabendu');
-nabs.addFamilyMember('Mousam');
-nabs.addFamilyMember('Hriday');
-nabs.addFamilyMember('Shikha');
-nabs.showFamily();
-mous.cleanRoom('Lizol');
+class TreboHotel extends Room {
+    private static instance: TreboHotel;
+    private constructor(room: string, private roomRent: number){
+        super(room);
+    }
+
+    static getInstance(){
+        if(!TreboHotel.instance)
+            TreboHotel.instance = new TreboHotel("Trebo", 1000)
+        return TreboHotel.instance
+    }
+
+    cleanRoom(soap: string) {
+        console.log(`${this.room} is Trebo Room cleaned with ${soap}`)
+    }
+}
+
+const vijay = TreboHotel.getInstance();
+console.log(vijay);
+
+const rohit = OyoRoom.createRoom('Rohit');
+console.log(rohit);
+console.log(OyoRoom.currentYear);
+const shekar = new OyoRoom('Shekar', 1000);
+const shobha = new OyoRoom('Shobha', 1000);
+const kapil = new OyoRoom('Kapil', 1000);
+shekar.newReport = 'Year End Report';
+console.log(shekar.allReports);
+shekar.addFamilyMember('Shekar');
+shekar.addFamilyMember('Shobha');
+shekar.addFamilyMember('Kapil');
+shekar.showFamily();
+shekar.showRoomRent();
+shekar.changeRoomRent(2000);
+shekar.showRoomRent();
+shekar.cleanRoom('Domex');
+
+// const nabs = new Room('Nabendu');
+// const mous = new Room('Mousam');
+// const hris = new Room('Hriday');
+// const shiks = new Room('Shikha');
+// shiks.dobShikha;
+// nabs.addFamilyMember('Nabendu');
+// nabs.addFamilyMember('Mousam');
+// nabs.addFamilyMember('Hriday');
+// nabs.addFamilyMember('Shikha');
+// nabs.showFamily();
+// mous.cleanRoom('Lizol');
